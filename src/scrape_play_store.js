5032a792c14e13888033231c5d17f32c1a938383
henriquepgomide <henriquepgomide@gmail.com>
2019-04-02 11:25:20 -0300

    Add code sample to fetch apps within a category

--- src/scrape_play_store.js
+++ src/scrape_play_store.js
@@ -3,7 +3,18 @@
 var gplay = require('google-play-scraper'); 
 
 // Define app id
-const appid = 'com.ea.game.pvz2_row';
+// const appid = 'com.ea.game.pvz2_row'; // Plant vs. Zombies
+const appid = 'com.king.candycrushsaga'; // Candy Crush Saga
+
+// Return list of applications of a given category
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    num: 120,
+    lang: 'pt',
+    country: 'br',
+  })
+  .then(console.log, console.log);
 
 // Returns app general information 
 gplay.app({appId: appid , 
@@ -13,14 +24,14 @@ gplay.app({appId: appid ,
 
 
 // Return categories available on Google Play
-gplay.categories().then(console.log);
+//gplay.categories().then(console.log);
 
 // Return app review
 gplay.reviews({
     appId: appid,
     lang: 'pt',
     page: 0,
-    sort: gplay.sort.RATING,
+    sort: gplay.sort.HELPFULNESS,
     page: 0,
 })
 //.then(console.log, console.log);
