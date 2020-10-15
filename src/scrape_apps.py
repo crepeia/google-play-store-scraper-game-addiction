c8a33ea1b5e5d62c422e237d503f948458a544fa
Henrique Gomide <henriquepgomide@gmail.com>
2020-10-15 17:06:22 -0300

    add new categories FAMILY, MUSIC, EDUCATION

--- src/scrape_apps.py
+++ src/scrape_apps.py
@@ -1,7 +1,4 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-from utils import store_apps_from_category 
+from utils import store_apps_from_category
 
 CATEGORIES = {
     "GAME_ACTION": "GAME_ACTION",
@@ -11,6 +8,9 @@ CATEGORIES = {
     "GAME_CARD": "GAME_CARD",
     "GAME_CASINO": "GAME_CASINO",
     "GAME_CASUAL": "GAME_CASUAL",
+    "GAME_EDUCATIONAL": "GAME_EDUCATIONAL",
+    "GAME_FAMILY": "GAME_FAMILY",
+    "GAME_MUSIC": "GAME_MUSIC",
     "GAME_PUZZLE": "GAME_PUZZLE",
     "GAME_RACING": "GAME_RACING",
     "GAME_ROLE_PLAYING": "GAME_ROLE_PLAYING",
@@ -22,10 +22,10 @@ CATEGORIES = {
 }
 
 if __name__ in '__main__':
-    try: 
+    try:
         for category in CATEGORIES:
             store_apps_from_category(category)
             print("Category {0} was stored.".format(category))
         print("Categories stored")
     except:
-        print("Something went ugly")
+        print("")
